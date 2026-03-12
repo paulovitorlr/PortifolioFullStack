91404bda9b1ab49276815f6e55ea8da6467e27d3
Paulo <paulovitorpesoal@gmail.com>
2026-03-12 15:07:22 -0300

    responsivo aba curriculo

--- portifolio/src/app/components/curriculo/curriculo.ts
+++ portifolio/src/app/components/curriculo/curriculo.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 //components
 import { NavBar } from '../nav-bar/nav-bar';
 import { Footer } from '../footer/footer';
+import { Navbarmobile } from '../navbarmobile/navbarmobile';
 //diretivas
 import { CurriculoDirective } from '../../directives/curriculo.directive';
 import { CurriculoCDirective } from '../../directives/curriculoColumn.directive';
 @Component({
   selector: 'app-curriculo',
-  imports: [NavBar,
+  imports: [
+    NavBar,
     Footer,
     CurriculoDirective,
-    CurriculoCDirective
+    CurriculoCDirective,
+    Navbarmobile
   ],
   templateUrl: './curriculo.html',
   styleUrl: './curriculo.scss',
